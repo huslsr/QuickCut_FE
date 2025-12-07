bb07cb3f30b41e1488846225ef0158be9cddc96c
ankit <2017araj@gmail.com>
2025-12-08 01:56:52 +0530

    fix

--- components/NewsFeed.tsx
+++ components/NewsFeed.tsx
@@ -17,7 +17,7 @@ export default function NewsFeed({ topStory, articles }: NewsFeedProps) {
           Latest Headlines
         </h2>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16">
           {articles.map((article) => (
             <StoryCard key={article.id} article={article} />
           ))}
