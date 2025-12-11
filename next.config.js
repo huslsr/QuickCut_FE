8601b339e49e9871203ac64b037669e5b96c1d62
ankit <2017araj@gmail.com>
2025-12-11 05:57:55 +0530

    fix: Update image remote patterns to support additional protocols and hostnames

--- next.config.js
+++ next.config.js
@@ -3,9 +3,25 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "tmpfiles.org",
+      },
+      {
+        protocol: "https",
+        hostname: "catbox.moe",
+      },
+      {
+        protocol: "https",
+        hostname: "files.catbox.moe",
+      }
     ],
   },
 };
