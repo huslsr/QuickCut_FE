9f54260bf53d6ab745432a204151d6f7bf14be43
ankit <2017araj@gmail.com>
2025-12-20 03:41:00 +0530

    google

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  /* 
-   * LOCAL: http://localhost:8080/api/v1 
-   * PROD: https://quickcutbe-production.up.railway.app/api/v1
-   */
-  baseURL:  'https://quickcutbe-production.up.railway.app/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
