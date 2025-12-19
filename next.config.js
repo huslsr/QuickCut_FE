18f14d63460fffc484380f4192fe3a498800c610
ankit <2017araj@gmail.com>
2025-12-20 04:38:44 +0530

    ddd

--- next.config.js
+++ next.config.js
@@ -30,7 +30,9 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // Proxy to Backend
+        destination: `${
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
+        }/:path*`, // Proxy to Backend
       },
     ];
   },
